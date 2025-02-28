e3340618017f338379c1a0bccd0869badef736cd
santiagoruiz313 <mosqueraericsantiago62@gmail.com>
2025-02-28 18:28:21 -0500

    avance 2

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 function abrirCalendario() {
     alert("Aquí se abrirá el calendario de reservas.");
 }
+// Mostrar el calendario al hacer clic en "Reservar Ahora"
+function mostrarCalendario() {
+    document.getElementById("calendario").style.display = "flex";
+}
+
+
