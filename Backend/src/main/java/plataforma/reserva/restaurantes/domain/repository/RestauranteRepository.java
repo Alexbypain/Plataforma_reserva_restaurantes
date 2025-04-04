56576d774bf1b351abe1b92e6ba696f6500f15ed
Danya-301 <danya.sierrrra@gmail.com>
2025-04-04 18:33:46 -0500

    se resuelven conflictos

--- Backend/src/main/java/plataforma/reserva/restaurantes/domain/repository/RestauranteRepository.java
+++ Backend/src/main/java/plataforma/reserva/restaurantes/domain/repository/RestauranteRepository.java
@@ -13,16 +13,6 @@ import plataforma.reserva.restaurantes.domain.entities.Restaurante;
 import plataforma.reserva.restaurantes.domain.entities.Usuario;
 
 
-public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
-
-    
-   Optional<Restaurante> findByAdministrador (Usuario usuarioId);
-    
-    
-import org.springframework.data.jpa.repository.JpaRepository;
-import plataforma.reserva.restaurantes.domain.entities.Restaurante;
-import plataforma.reserva.restaurantes.domain.entities.Usuario;
-
-public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
-    Restaurante findByAdministrador(Usuario admin);
-}
+public interface RestauranteRepository extends JpaRepository<Restaurante,Long> { 
+   Restaurante findByAdministrador (Usuario usuarioId);
+}   
